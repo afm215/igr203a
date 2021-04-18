72a5a7070bd653478e4d6d88d3ba6b16e1dedd11
Lucas <lucas.teissier@yahoo.fr>
2021-04-18 16:56:53 +0200

    fragments

--- AndroidApp/app/src/main/java/com/example/carte/FragmentMenu.java
+++ AndroidApp/app/src/main/java/com/example/carte/FragmentMenu.java
@@ -1,14 +1,10 @@
 package com.example.carte;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,32 +17,33 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class FragmentCarte extends Fragment {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+import com.google.android.material.navigation.NavigationView;
+
+public class FragmentMenu extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_carte, container, false);
+        View root = inflater.inflate(R.layout.fragment_menus, container, false);
 
         BottomNavigationView navView = root.findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_entrees, R.id.navigation_plats, R.id.navigation_desserts,R.id.navigation_boissons)
+                R.id.navigation_menu1, R.id.navigation_menu2, R.id.navigation_menu3, R.id.navigation_menu4)
                 .build();
-        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_carte);
+        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_menu);
         NavGraph graph = navController.getGraph();
-        graph.setStartDestination(R.id.navigation_entrees);
+        graph.setStartDestination(R.id.navigation_menu1);
         navController.setGraph(graph);
-
 
         NavigationUI.setupActionBarWithNavController((AppCompatActivity) getActivity(), navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
         navView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
-
-
 
         return root;
     }
---
+++ AndroidApp/app/src/main/java/com/example/carte/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.carte;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_carte, R.id.navigation_menus)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavGraph graph = navController.getGraph();
+        graph.setStartDestination(R.id.navigation_carte);
+        navController.setGraph(graph);
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_cartes:
+
+                break;
+
+            case R.id.nav_menus:
+
+                break;
+        }
+        return false;
+    }
+}
